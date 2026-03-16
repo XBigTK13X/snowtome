713c365c3034fae653fefc9a49c72b8c0665fa14
Lofty Snowman <xbigtk13x@gmail.com>
2026-03-16 10:47:00 -0500

    Working on prod generate apks

--- script/prod-generate-apks.sh
+++ script/prod-generate-apks.sh
@@ -2,8 +2,10 @@
 
 echo "THIS IS THE ONE TO USE!"
 
+source script/variables.sh
+
 cd expo/android
 mkdir -p build-out
-export EXPO_TV=1
+export EXPO_TV=0
 ./gradlew assembleRelease
-cp app/build/outputs/apk/release/app-release.apk build-out/snowtome-tv.apk
+cp app/build/outputs/apk/release/app-release.apk build-out/snowtome-mobile.apk
