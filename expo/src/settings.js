b664b22748bb6d979b3d11ddedae293bf3085c7a
XBigTK13X <xbigtk13x@gmail.com>
2026-03-29 11:37:24 -0500

    Upgrade snowui

--- expo/src/settings.js
+++ expo/src/settings.js
@@ -1,7 +1,7 @@
 class Config {
     constructor() {
-        this.clientVersion = "1.2.4"
-        this.clientBuildDate = "March 17, 2026"
+        this.clientVersion = "1.2.5"
+        this.clientBuildDate = "March 29, 2026"
         this.clientDevBuildNumber = 1
         this.debugSnowUi = false
     }
